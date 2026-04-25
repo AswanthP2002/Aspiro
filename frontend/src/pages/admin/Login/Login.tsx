5f33410c04ff4dcc072351a95f861a2c9f39bda0
AswanthP2002 <aswanth.prathayoth@gmail.com>
2026-04-25 12:39:06 +0530

    FIX:Improved admin login ui game a polished look

--- frontend/src/pages/admin/Login/Login.tsx
+++ frontend/src/pages/admin/Login/Login.tsx
@@ -57,11 +57,6 @@ export default function AdminLoginPage(){
                 userToken: result.result.accessToken,
                 userRole: result.result.role
             }))
-            // dispatcher(loginSuccess({
-            //     user:result?.result?.user,
-            //     userToken:result?.result?.accessToken,
-            //     userRole:result?.result?.role
-            // }))
             setLoginErrorText('')
             // setloading(false)
             navigate('/admin/dashboard')
@@ -82,15 +77,15 @@ export default function AdminLoginPage(){
                             <p className='text-gray-700'>Aspiro</p>
                         </div>
                         <div className='mt-2'>
-                            <p className='text-gray-700'>Admin Portal</p>
-                            <p className='mt-2 text-sm text-gray-500'>Sign in to access the admin dashboard</p>
+                            <p className='text-2xl font-bold text-gray-900 tracking-wider'>Admin Portal</p>
+                            <p className='mt-2 text-gray-600'>Sign in to access the admin dashboard</p>
                         </div>
                         <div className='w-full'>
                             <p className='text-center text-red-500 text-sm font-light !mt-5'>{loginErrorText}</p>
                         </div>
                         <form onSubmit={handleSubmit(adminLoginOnSubmit)} className='mt-5'>
                             <FormControl fullWidth error={Boolean(errors.email)}>
-                                <label htmlFor="" className='text-xs font-medium !text-black'>Email Address</label>
+                                <label htmlFor="" className='uppercase font-semibold tracking-wide mb-2 !text-gray-500'>Email Address</label>
                                 <Controller
                                     name='email'
                                     control={control}
@@ -98,12 +93,12 @@ export default function AdminLoginPage(){
                                         required:{value:true, message:'Enter email'}
                                     }}
                                     render={({field}) => (
-                                        <div className='flex gap-2 items-center bg-gray-100 !p-2 rounded-sm'>
-                                            <HiOutlineEnvelope color='gray' />
+                                        <div className='flex gap-2 items-center relative'>
+                                            <HiOutlineEnvelope color='gray' className='absolute left-3' />
                                             <input type='text'
                                                 {...field}
                                                 placeholder='Enter admin email address'
-                                                className='w-full'
+                                                className='w-full border !border-slate-100 px-10 py-3 bg-gray-50 focus:bg-white rounded-md focus:!border-blue-300 focus:ring-2 focus:ring-blue-100'
                                             />
                                         </div>
                                     )}
@@ -114,7 +109,7 @@ export default function AdminLoginPage(){
                             </FormControl>
 
                             <FormControl fullWidth error={Boolean(errors.password)} className='!mt-3'>
-                                <label htmlFor="" className='text-xs font-medium !text-black'>Password</label>
+                                <label htmlFor="" className='uppercase mb-2 font-semibold tracking-wide'>Password</label>
                                 <Controller 
                                     name='password'
                                     control={control}
@@ -122,11 +117,12 @@ export default function AdminLoginPage(){
                                         required:{value:true, message:'Enter your password'}
                                     }}
                                     render={({field}) => (
-                                        <div className='bg-gray-100 relative flex gap-2 !p-2 items-center'>
-                                            <GoLock color='gray' />
+                                        <div className='relative flex gap-2 items-center'>
+                                            <GoLock color='gray' className='absolute left-3' />
                                             <input type={`${showPassword ? "text": "password"}`} 
                                                 {...field}
                                                 placeholder='Enter your password'
+                                                className='border !border-slate-100 px-10 py-3 bg-gray-50 focus:bg-white rounded-md w-full focus:!border-blue-300 focus:ring-2 focus:ring-blue-100'
                                             />
                                             {showPassword
                                                 ? <button type='button' onClick={() => setShowPassword(false)} className="absolute right-3"><FiEyeOff /></button>
@@ -138,7 +134,7 @@ export default function AdminLoginPage(){
                                 <FormHelperText>{errors.password?.message}</FormHelperText>
                             </FormControl>
                             <div className='mt-4'>
-                                <button type='submit' className='bg-gradient-to-br from-blue-500 to-indigo-700 w-full !p-2 rounded-sm text-sm text-white'>Sign In</button>
+                                <button type='submit' className='border border-transparent w-full p-3 rounded-lg bg-gradient-to-br from-blue-400 to-indigo-500 text-white font-semibold transition-color duration-300 shadow-xl hover:shadow-2xl'>Sign In</button>
                             </div>
                         </form>
                         <div className="info !mt-10 bg-blue-50 !p-3 flex gap-2">
@@ -154,7 +150,7 @@ export default function AdminLoginPage(){
                 </div>
                 <div className=" relative right hidden md:flex flex-col items-center justify-center bg-gradient-to-br from-blue-800 via-blue-300 to-blue-800">
                     <div className="!z-10 w-full flex flex-col justify-center items-center">
-                        <p className='text-white'>Welcome Back</p>
+                        <p className='text-white font-bold text-3xl tracking-wide'>Welcome Back</p>
                     <p className='mt-5 text-white text-center text-sm'>Manage your platform with powerful admin tools and insights</p>
                     <div className="cards flex gap-3 mt-10">
                         <div className='bg-gradient-to-br from-blue-500 to-indigo- shadow-lg hover:-translate-y-1 transition-all duration-2s ease-in-out rounded-md text-center !px-5 !py-3 w-1/3 text-white'>
@@ -171,89 +167,10 @@ export default function AdminLoginPage(){
                         </div>
                     </div>
                     </div>
-                    <div className="rounded-full bg-indigo-400 w-[300px] h-[300px] absolute">
+                    <div className="rounded-full bg-indigo-400 w-[300px] h-[300px] absolute ring-2 ring-blue-400 shadow-[0_0_20px_2px_rgba(0,0,260,0.5)]">
                     </div>
                 </div>
             </div>
         </>
-    //     <div className="main w-full h-screen bg-secondary flex items-center justify-center">
-    //         {loading ? <InfinitySpinner color='orange' /> : null}
-    //     <div className="max-w-4xl w-full bg-white flex flex-col md:flex-row shadow-sm">
-    //         <div id="login-section" className='md:w-1/2 p-8 bg-white'>
-    //             <p className="brand-name">Aspiro</p>
-    //             <div className="flex items-center justify-center">
-    //                 <h2 className='font-bold'>Login</h2>
-    //             </div>
-    //             <div className='flex justify-center'>
-    //                 {loginError ? <label htmlFor="" className='error-label'>{loginErrorText}</label> : null}
-    //             </div>
-    //             <form className="form" onSubmit={handleSubmit(onSubmit)}>
-    //                 <FormControl fullWidth sx={{marginTop:'10px'}}>
-    //                     <Controller
-    //                         name='email'
-    //                         control={control}
-    //                         rules={{
-    //                             required:{value:true, message:'Please enter email'}
-    //                         }}
-    //                         render={({field}) => {
-    //                             return <TextField
-    //                                 {...field}
-    //                                 label="Email"
-    //                                 variant='outlined'
-    //                                 error={Boolean(errors.email)}
-    //                                 helperText={errors.email?.message}
-    //                             />
-    //                         }}
-    //                     />
-    //                 </FormControl>
-                    
-    //                 <FormControl fullWidth sx={{marginTop:'10px'}}>
-    //                     <Controller
-    //                         name='password'
-    //                         control={control}
-    //                         rules={{
-    //                             required:{value:true, message:'Enter password'}
-    //                         }}
-    //                         render={({field}) => {
-    //                             return <TextField
-    //                                 {...field}
-    //                                 type={showPassword ? 'text' : 'password'}
-    //                                 label="Password"
-    //                                 variant='outlined'
-    //                                 error={Boolean(errors.password)}
-    //                                 helperText={errors.password?.message}
-    //                                 InputProps={{
-    //                                     endAdornment: (
-    //                                         <InputAdornment position="end">
-    //                                             <IconButton
-    //                                                 onClick={() => setShowPassword(!showPassword)}
-    //                                                 edge="end"
-    //                                             >
-    //                                                 <i className={showPassword ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"}></i>
-    //                                             </IconButton>
-    //                                         </InputAdornment>
-    //                                     )
-    //                                 }}
-    //                             />
-    //                         }}
-    //                     />
-    //                 </FormControl>
-                    
-    //                 <div className='flex justify-end'>
-    //                     <Link to={'/forgot-password'} className='forgot-password mt-4 mb-4 block'>Forgot password?</Link>
-    //                 </div>
-    //                 <div>
-    //                     <button type="submit" className='bg-main w-full rounded-full text-white py-2'>Login</button>
-    //                 </div>
-    //             </form>
-    //         </div>
-    //         <div id="welcome-section" className='flex items-end justify-start md:w-1/2 bg-main text-white relative'>
-    //             <p className="font-3xl mx-8 my-8" id='welcome-text'>Hi<br />Welcome<br /> back</p>
-    //             <div className="rounded-full dec-circle absolute right-8 bottom-10"></div>
-    //             <div className="rounded-full dec-circle-top-1 absolute top-4 left-4"></div>
-                
-    //         </div>
-    //     </div>
-    // </div>
     )
 }
